d3078d93122b38908a7e7d038d3433fcdb220e25
Gaurav <gaurav.joshi@utdallas.edu>
2022-01-04 21:24:21 -0600

    Fixed DFS solution for CourseSchedule.java

--- Grokking/Topological Sort (Graph)/CourseSchedule.java
+++ Grokking/Topological Sort (Graph)/CourseSchedule.java
@@ -1,6 +1,8 @@
 class Solution {
     HashMap<Integer, List<Integer>> courseMap = new HashMap<>();
+    HashMap<Integer, Boolean> memo = new HashMap<>(); //optimize DFS pruning
     Set<Integer> visited = new HashSet<>();
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         // construct the hashmap
         for (int[] listOfCourses : prerequisites) {
@@ -8,35 +10,38 @@ class Solution {
             int prereq = listOfCourses[1];
 
             if (!courseMap.containsKey(course)) {
-                List<Integer> prereqList = new ArrayList<>();
-                prereqList.add(prereq);
-                courseMap.put(course, prereqList);
-            } else {
-                List<Integer> prereqList = courseMap.get(course);
-                prereqList.add(prereq);
-                courseMap.put(course, prereqList);
+                courseMap.put(course, new ArrayList<>());
             }
+
+            courseMap.get(course).add(prereq);
+
         }
 
-        for (int i = 0; i < numCourses; i++) {
-            if (isCyclic(i)) return false;
+        for (int[] listOfCourses : prerequisites) {
+            if (isCyclic(listOfCourses[0], -1)) return false;
         }
 
         return true;
     }
 
-    private boolean isCyclic(int course) {
+    private boolean isCyclic(int course, int prereqCourse) {
+
+        // if the path has already been traversed, return previous output
+        if (memo.containsKey(course)) return memo.get(course);
+
         // key does not exist in the map meaning no pre-reqs
-        if (courseMap.get(course) == null) return false;
+        if (!courseMap.containsKey(course)) return false;
 
         // loop detected
         if (visited.contains(course)) return true;
 
         visited.add(course);
-
         // recursively check if any courses that require current course produce cycles
         for (Integer prereq : courseMap.get(course)) {
-            if (isCyclic(prereq)) return true;
+            boolean hasCycle = isCyclic(prereq, course);
+            memo.put(prereq, hasCycle);
+
+            if (hasCycle) return true;
         }
 
         visited.remove(course);
