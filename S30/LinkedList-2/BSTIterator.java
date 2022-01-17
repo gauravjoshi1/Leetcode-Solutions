7810f981652e6bb15850adcc9da7d631f27e129f
Gaurav <gaurav.joshi@utdallas.edu>
2022-01-16 23:38:55 -0600

    Updated Solution for BST Iterator

---
+++ S30/LinkedList-2/BSTIterator.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    Stack<TreeNode> bStack;
+    
+    public BSTIterator(TreeNode root) {
+        bStack = new Stack<>();
+        
+        // call the inorderT to create an inorder List
+        leftTraverse(root);
+    }
+    
+    public int next() {
+        TreeNode currNode = bStack.pop();
+        
+        if (currNode.right != null) {
+            leftTraverse(currNode.right);    
+        }
+        
+        return currNode.val;
+    }
+    
+    public boolean hasNext() {
+        return !bStack.isEmpty();
+    }
+    
+    private void leftTraverse(TreeNode ptr) {
+        while (ptr != null) {
+            bStack.push(ptr);
+            ptr = ptr.left;
+        }
+    }
+    
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
